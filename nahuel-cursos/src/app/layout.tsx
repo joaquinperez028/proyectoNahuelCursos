73fbdc7c2df72224c0de92a78cf9c8a53c4236de
Cuutu <32869813+Cuutu@users.noreply.github.com>
2025-04-07 15:51:53 -0300

    Cambiado botón a color negro

--- nahuel-cursos/src/app/layout.tsx
+++ nahuel-cursos/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <Providers>
           <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="flex-grow">
+            <main className="flex-grow pt-16">
               {children}
             </main>
             <Footer />
