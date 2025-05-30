d45d64e1dd6c4a28b3299ac23ef00f68c1138c04
joaquinperez028 <tortuwu028@gmail.com>
2025-05-30 01:53:37 -0300

    cambiando cosas del perfi del usuario

--- proyectonahuel/components/UserStats.tsx
+++ proyectonahuel/components/UserStats.tsx
@@ -64,7 +64,7 @@ export default function UserStats({
         value={totalCourses}
         bgClass="bg-gradient-to-r from-[#007bff] to-[#0056b3]"
         icon={
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#007bff]" fill="currentColor" viewBox="0 0 24 24">
             <path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6L23 9l-11-6zM18.82 9L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z"/>
           </svg>
         }
@@ -75,7 +75,7 @@ export default function UserStats({
         value={completedCourses}
         bgClass="bg-gradient-to-r from-[#4CAF50] to-[#388E3C]"
         icon={
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4CAF50]" fill="currentColor" viewBox="0 0 24 24">
             <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/>
           </svg>
         }
@@ -86,7 +86,7 @@ export default function UserStats({
         value={certificatesEarned}
         bgClass="bg-gradient-to-r from-[#ff9800] to-[#f57c00]"
         icon={
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#ff9800]" fill="currentColor" viewBox="0 0 24 24">
             <path d="M12 2l2.09 6.26L20 10l-5.91 1.74L12 18l-2.09-6.26L4 10l5.91-1.74L12 2zm0 4.33l-.89 2.67L8.44 10l2.67.89L12 13.56l.89-2.67L15.56 10l-2.67-.89L12 6.33z"/>
           </svg>
         }
